674ee6d1f273daff46b475c571673616e26e3a9f
daonghia2404 <daonghia2404@gmail.com>
2022-11-09 15:22:55 +0700

    Fix bgus

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,6 +1,7 @@
 window.onload = () => {
   buttonOnTop.init();
   arrangeEvents.init();
+  storageCollpase.init();
 };
 
 const buttonOnTop = {
@@ -67,5 +68,57 @@ const arrangeEvents = {
         });
       });
     });
+  },
+};
+
+const storageCollpase = {
+  init: function () {
+    this.config();
+  },
+  config: function () {
+    const key = "collapses";
+    const collapses = Array.from(
+      document.querySelectorAll(".Accordion .collapse")
+    );
+
+    const initialCollapseStorage = collapses.map(
+      (collapse) => `#${collapse.id}`
+    );
+
+    const storageBefore = localStorage.getItem(key)
+      ? JSON.parse(localStorage.getItem(key) || "[]")
+      : initialCollapseStorage;
+
+    if (!localStorage.getItem(key)) {
+      localStorage.setItem(key, JSON.stringify(initialCollapseStorage));
+    }
+
+    collapses.forEach((collapse) => {
+      if (storageBefore.includes(`#${collapse.id}`)) collapse.classList.add("show");
+      else collapse.classList.remove("show");
+    });
+
+    const triggerCollapsesBtn = document.querySelectorAll(
+      '[data-toggle="collapse"]'
+    );
+
+    triggerCollapsesBtn.forEach((btn) =>
+      btn.addEventListener("click", () => {
+        const storage = JSON.parse(localStorage.getItem(key) || "[]");
+
+        const targetId = btn.dataset.target;
+        const isCollapsed = !document
+          .querySelector(targetId)
+          .className.includes("show");
+
+        if (isCollapsed) {
+          const newStorage = [...storage, targetId];
+          localStorage.setItem(key, JSON.stringify(newStorage));
+        } else {
+          const newStorage = storage.filter((item) => item !== targetId);
+          localStorage.setItem(key, JSON.stringify(newStorage));
+        }
+      })
+    );
   },
 };
